bfbb2a088655344b40fedf66b709969faff92df5
root <info@eco-global.io>
2021-09-03 01:00:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ECOP Protocol
+=================
+
+The ECOP Protocol is an Ethereum smart contract for supplying or borrowing assets. Through the cToken contracts, accounts on the blockchain *supply* capital (Ether or ERC-20 tokens) to receive cTokens or *borrow* assets from the protocol (holding other assets as collateral). The ECOP cToken contracts track these balances and algorithmically set interest rates for borrowers.
+
+
+_© Copyright 2020, ECOP Labs_
---
+++ contracts/PriceOracle.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.16;
+
+import "./EToken.sol";
+
+contract PriceOracle {
+    /// @notice Indicator that this is a PriceOracle contract (for inspection)
+    bool public constant isPriceOracle = true;
+
+    /**
+      * @notice Get the underlying price of a eToken asset
+      * @param eToken The eToken to get the underlying price of
+      * @return The underlying asset price mantissa (scaled by 1e18).
+      *  Zero means the price is unavailable.
+      */
+    function getUnderlyingPrice(EToken eToken) external view returns (uint);
+}
